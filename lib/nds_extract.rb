bed304ffcb3fe42ee1c87bb3accca14a2c5feb01
Kelly Powers <kpowers.nj@gmail.com>
2020-01-05 19:40:54 +0000

    Automatically backed up by Learn

--- lib/nds_extract.rb
+++ lib/nds_extract.rb
@@ -5,12 +5,51 @@ require 'directors_database'
 #
 # { directorOne => allTheMoneyTheyMade, ... }
 
+
+
+# def directors_totals(nds)
+#   result = {}
+#   index1 = 0 
+#   while index1 < nds.length do 
+#     dir_name = nds[index1][:name]
+#     index2 = 0 
+#       while index2 < nds[index][:name][index2][:worldwide_gross] do 
+        
+#     index1 += 1 
+#     result[dir_name] = gross_for_director(director_data)
+
+#   #nil
+# end
+
 def directors_totals(nds)
-  result = {}
-  nil
+  directors_totals = {}
+  index1 = 0 
+  row_index = 0 
+  while row_index < nds.length do 
+    dir_name = nds[row_index][:name]
+    column_index = 0 
+    dir_total = 0 
+  #^this is where we will pull the name from [:name]
+    while column_index < nds[row_index][:movies].length do 
+      dir_gross = nds[row_index][:movies][column_index][:worldwide_gross]
+      dir_total += dir_gross
+      column_index += 1 
+      # puts "row is #{row_index} column is #{column_index}"
+    end 
+    directors_totals[dir_name] = dir_total
+    row_index += 1 
+  end 
+ puts directors_totals
+ directors_totals
 end
+
+
+def gross_for_director(director_data)
+  
+  
+
+end
+
 
 # Find a way to accumulate the :worldwide_grosses and return that Integer
 # using director_data as input
-def gross_for_director(director_data)
-end
